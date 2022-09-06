ce93aae4744f9e93c5d9c1095f755e686293a121
Mauco <maucopozo23@gmail.com>
2022-09-06 03:18:50 -0300

    desafio1

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+const NavBar = () => {
+  return (
+<header>
+    <div>
+      <h1>MUNDIALITO</h1>
+    </div>
+    <div>
+      <ul>
+        <li> <a href="index.html">TODO</a></li>
+        <li> <a href="index.html">PAISES</a></li>
+        <li> <a href="index.html">JUGADORES</a></li>
+        <li> <a href="index.html">ESTADIOS</a></li>
+      </ul>
+    </div>
+</header>
+
+  );
+
+}
+
+export default NavBar;
